28ef228b365c1feeafd37eacac1390fb8b931bf8
Maxim Drachev <drachyov.maxim@gmail.com>
2021-06-17 00:19:17 +0400

    update

--- js/index.js
+++ js/index.js
@@ -1,53 +1,13 @@
-document.addEventListener('DOMContentLoaded', function(event) {
-  var room = document.getElementById('room');
-  var cellCount = 4;
-  var selectedIndex = 0;
-
-  function rotateRoom() {
-    var angle = selectedIndex / cellCount * -360;
-    room.style.transform = 'rotateY(' + angle + 'deg)';
-  }
-
-  var prevArrow = document.getElementById('arrow-left');
-  prevArrow.addEventListener( 'click', function() {
-    selectedIndex++;
-    rotateRoom();
-  });
-
-  var nextArrow = document.getElementById('arrow-right');
-  nextArrow.addEventListener( 'click', function() {
-    selectedIndex--;
-    rotateRoom();
-  });
-
-  var infoButton = document.getElementById('info-link');
-  var infoWrapper = document.getElementById('info-wrapper');
-  var body = document.getElementById('window');
-  infoButton.addEventListener( 'click', function() {
-    if (infoWrapper.classList.contains("hidden")) {
-      infoWrapper.classList.remove("hidden");
-    } else {
-      infoWrapper.classList.add("hidden");
-    }
-  });
-  body.addEventListener( 'click', function() {
-    if (infoWrapper.classList.contains("hidden")) {
-      return false;
-    } else {
-      infoWrapper.classList.add("hidden");
-    }
-  });
-
-  var zoom = document.getElementsByClassName("wall");
-  var i;
-
-  for (i = 0; i < zoom.length; i++) {
-    zoom[i].addEventListener("click", function() {
-      if (body.classList.contains("zoomed")) {
-        body.classList.remove("zoomed");
-      } else {
-        body.classList.add("zoomed");
-      }
-    });
-  }
-});
+function scrollHandler () { 
+  // how far the user has scrolled so far on the page 
+  const scrolled = window.innerHeight + window.scrollY; 
+  // height of the document 
+  const documentHeight = document.body.offsetHeight; 
+  // the amount of distance in pixels needed from the bottom of the page before the page scrolls to the top 
+  const offset = 100; 
+  if (offset + scrolled >= documentHeight) 
+ 
+    window.scrollTo(0,0); 
+} 
+ 
+window.addEventListener('scroll', scrollHandler); 
